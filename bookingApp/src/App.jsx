b33228571afe9a60eb878eb6c0d7a117c726e7a3
YaffetK <yaffet97@gmail.com>
2023-01-10 16:01:03 +0100

    add login in form

--- bookingApp/src/App.jsx
+++ bookingApp/src/App.jsx
@@ -1,7 +1,29 @@
-
+import { useState } from 'react'
 import './index.css'
 
 function App() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = async () => {
+
+    try {
+      if (email) {
+        console.log('Login success')
+      }
+      fetch()
+
+
+
+      displayBoxes()
+      console.log('Boxar genereras')
+    }
+    catch (error) {
+      console.error(error)
+
+    }
+  }
 
 
   return (
@@ -19,6 +41,41 @@ function App() {
         </nav>
 
         <h1 className=' font-bold text-center'> Hello World</h1>
+
+        <div className='flex justify-end'>
+          <form>
+            <div className="login-form-wrapper">
+
+              <div className="input-holder">
+                <label htmlFor="email">Email</label>
+                <input id="email"
+                  type='text'
+                  placeholder='JohnDoe@gmail.com'
+                  onChange={(e) => {
+                    setEmail(e.target.value)
+                  }}
+                  value={email}
+                  required
+                />
+              </div>
+
+              <div className="text-slate-100 text-1xl">
+                <label htmlFor="password">Password</label>
+                <input id="password"
+                  type='password'
+                  placeholder='JohnDoe@gmail.com'
+                  value={password}
+                  onChange={(e) => {
+                    setPwd(e.target.value)
+                  }}
+                  required
+                />
+              </div>
+
+            </div>
+            <button className="bg-blue-200" onClick={handleSubmit} type='submit' >Submit</button>
+          </form>
+        </div>
 
       </section>
 
