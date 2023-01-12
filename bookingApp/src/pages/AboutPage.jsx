1904059574a5f68e0e0836cf320d388ad03cb9c9
YaffetK <yaffet97@gmail.com>
2023-01-12 19:33:51 +0100

    WIP

---
+++ bookingApp/src/pages/AboutPage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from '../components/Navbar'
+
+const AboutPage = () => {
+
+
+
+
+    return (
+
+        <div>
+
+            <nav>
+                <Navbar />
+
+            </nav>
+
+
+
+
+            <div>This is the about page</div>
+        </div>
+    )
+};
+
+export default AboutPage;
